a4e09b7d2a507097d865af07f1208911e8145426
Diego Jesus Posada martinez <djpmz24@gmail.com>
2020-01-22 00:02:21 -0500

    nueva version

---
+++ resources/views/home1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    
+    <h1>Home</h1>
+
+    <header>
+        <nav>
+            <a href= "<?php echo route('home1')?> "> Inicio</a>
+            <a href= "<?php echo route('contactos')?> "> Saludar contacto</a>
+        </nav>
+
+
+    </header>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', ['as' => 'home1' ,function(){
+   // echo "<a href= ". route('contactos') . "> Contacto </a><br> ";  Ejemplo de como hacer llamado
+   return view('welcome');
+}]);
+
+Route::get('contactanos', ['as'=>'contactos', function(){  //se le da nombre a la ruta. Nombre: contactos
+    return view('saludo');
+
+}]);
+
+Route::get('saludos/{nombre?}', function($nombre = "invitado"){  //el ? es para cuando no se pasa el parametro. Se pasa "invitado" por defecto
+    return "hola! $nombre"; 
+
+})->where('nombre', "[A-Za-z]+");
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
